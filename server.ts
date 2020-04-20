a0ff4682f21156dddbf1f272bed7ec17c8f39e8d
Ricardo Gomes <desk467@gmail.com>
2020-04-20 00:51:21 -0300

    fix: Ajustar server para receber JSON como body nas requisições

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
+import * as Express from 'express'
 import { app } from './app'
 import registerControllers from './register-controllers'
 
+app.use(Express.json())
 registerControllers(app)
 
 console.log(`[WEBSERVICE] Webservice ativo na porta ${process.env.PORT}`)
