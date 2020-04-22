9a48ae67beef3c080aad0f39f555328f0b9ec150
Ricardo Gomes <desk467@gmail.com>
2020-04-21 22:51:30 -0300

    feat: Logar erros das requisições em um arquivo

---
+++ logger.ts
@@ -0,0 +1,14 @@
+import * as winston from 'winston'
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({
+            filename: 'errors.log',
+            level: 'error',
+        })
+    ]
+})
+
+export default logger
--- middlewares/error-handler.ts
+++ middlewares/error-handler.ts
@@ -1,7 +1,11 @@
 import * as Express from 'express'
+import logger from '../logger'
 
 export default function ErrorHandler(err: Error, req: Express.Request, res: Express.Response, next: Express.NextFunction) {
-    console.log(err)
+    logger.error({
+        error: err,
+        stack: err.stack,
+    })
 
     res.status(500).json({ message: 'Something failed :/' })
 }
