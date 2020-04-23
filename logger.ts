7a8857eb1f0457d5e044f31b101238a595b6c93f
Ricardo Gomes <desk467@gmail.com>
2020-04-22 23:15:28 -0300

    feat: Adicionar transport Console ao Logger

--- logger.ts
+++ logger.ts
@@ -4,6 +4,9 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
+        new winston.transports.Console({
+            level: 'info',
+        }),
         new winston.transports.File({
             filename: 'errors.log',
             level: 'error',
